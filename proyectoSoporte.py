50e974139ee3923347add1fbf91e991508cc7037
Manuel Lopez <51802110+newgig1234@users.noreply.github.com>
2020-10-26 15:22:06 -0300

    Update proyectoSoporte.py

--- proyectoSoporte.py
+++ proyectoSoporte.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import Boolean
+from sqlalchemy.ext.declarative import declarative_base
 
+Base= declarative_base()
+
+class User(Base):
+    __tablename__:'user'
+    
+        
