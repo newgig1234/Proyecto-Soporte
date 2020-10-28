27515810ee0730b101bc310fa97ee01fb1b2038e
Manuel Lopez <manu1475963@gmail.com>
2020-10-28 11:22:10 -0300

    Base de saros funcionando

--- Datos.py
+++ Datos.py
@@ -1,33 +1,27 @@
 #from typing import final
 import sqlalchemy
 from sqlalchemy import Column, String, Integer, ForeignKey,Date,Time,MetaData
-from sqlalchemy.orm import relationship,backref
+from sqlalchemy.orm import backref, relationship
 #from sqlalchemy.orm.relationships import foreign
-from sqlalchemy.sql.sqltypes import Boolean
+from sqlalchemy.sql.sqltypes import Boolean, DateTime, INTEGER
 from sqlalchemy.sql.schema import Table 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 
-Base= declarative_base()
-
 engine= create_engine('sqlite:///datos.db', echo=True)
-Base.metadata.create_all(engine)
+Base = declarative_base()
 
-engine = create_engine('sqlite:///:memory:')
-Base.metadata.bind = engine
-db_session = sessionmaker()
-db_session.bind = engine
-session = db_session()
+
 
 class Alumno(Base):
     __tablename__='alumnos'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(INTEGER, primary_key=True, autoincrement=True, nullable=False)
     nombre = Column(String)
     apellido = Column(String)
     email = Column(String)
-    legajo = Column(Integer,nullable=False) #id login
+    legajo = Column(INTEGER,nullable=False) #id login
     #username = Column(String, nullable=False)
     password = Column(String, nullable=False)
 
@@ -39,13 +33,14 @@ class Alumno(Base):
 
 class Materia(Base):
     __tablename__ = 'materias'
-    idMateria = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(INTEGER, primary_key=True, autoincrement=True, nullable=False)
     nombreMateria = Column(String)
     añoCursado = Column(String)
     profesor = Column(String)
     email_profesor = Column(String)
     tipoMateria = Column(String) #si es electiva o no
 
+    comision= relationship('Comision',secondary='alumnoclase')
 
     def __init__(self,nombreMateria,año,profesor,email_profesor,tipoMateria):
         self.nombreMateria = nombreMateria
@@ -67,9 +62,12 @@ class Materia(Base):
 class Comision(Base):
     #cambiar en md
     __tablename__ = 'comisiones'
-    idComision = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(INTEGER, primary_key=True, autoincrement=True, nullable=False)
     descripcion = Column(String)
-    cicloLectivo = Column(Integer, nullable=False)
+    cicloLectivo = Column(INTEGER)
+    numeroComision=Column(INTEGER)
+
+    materia=relationship('Materia',secondary='alumnoclase')
     
     def __init__(self,descripcion,cicloLectivo):
         self.descripcion = descripcion
@@ -78,26 +76,34 @@ class Comision(Base):
     def __repr__(self):
         pass
 
-class Clase(Base):
-    __tablename__ = 'clases'
-    idClase=Column(Integer,PrimaryKey=True,autoincrement=True,nullable=False)
+class AlumnoComisionMateria(Base):
+    __tablename__='alumnocomisionmateria'
+    alumno_id=Column(INTEGER,ForeignKey('alumnos.id'),primary_key=True)
+    comision_materia_id=Column(INTEGER,ForeignKey('comisionmateria.comision_materia_id'),primary_key=True)
     
-    comision_id = Column(Integer, ForeignKey('comisiones.idComision'), primary_key=True)
-   
-    materia_id = Column(Integer, ForeignKey('materias.idMateria'), primary_key=True)
+    def __repr__(self):
+        pass
 
+class ComisionMateria(Base):
+    __tablename__='comisionmateria'
+    comision_materia_id=Column(Integer,primary_key=True)
+    comision_id=Column(INTEGER,ForeignKey('comisiones.id'),primary_key=True)
+    materia_id=Column(INTEGER,ForeignKey('materias.id'),primary_key=True)
+    #extra data
     horarioTeoria = Column(String, nullable=False)
     diaTeoria = Column(String, nullable=False)
     aulaTeoria = Column(String, nullable=False)
     horarioPractica = Column(String, nullable=True)
     diaPractica = Column(String, nullable=True)
     aulaPractica = Column(String, nullable=True)
+    fechaInscripcion=Column(DateTime)
 
-    alumnos = relationship('Alumnos', secondary='AlumnoClase', backref='clases')
+    comision=relationship('Materia',back_populates='comisiones')
+    materia=relationship('Comision',back_populates='materias')
 
-    def __init__(self,com_id,mat_id,horaT,diaT,aulaT,horaP,diaP,aulaP):
-        self.comision_id = com_id
-        self.materia_id = mat_id
+
+
+    def __init__(self,horaT='7:00-9:00',diaT='Lunes',aulaT='404',horaP='9:00-11:00',diaP='Martes',aulaP='505'):
         self.horarioTeoria = horaT
         self.diaTeoria = diaT
         self.aulaTeoria = aulaT
@@ -109,39 +115,4 @@ class Clase(Base):
         return f'Clase {self.comision_id}{self.materia_id} Teoria {self.diaTeoria} {self.horarioTeoria} Practica {self.diaPractica}{self.horarioPractica}'
 
 
-relacion_alumno_clase = Table('AlumnoClase',
-    Column('idRelacion',Integer,primary_key=True),
-    Column('alumnoId',Integer,ForeignKey('alumnos.id')),
-    Column('claseId',Integer,ForeignKey('clases.idClase'))
-    )
-
-
-
-
-
-# class Modulo(Base):
-#     __tablename__ = 'modulos'
-#     #dias y horarios en los que se cursa, en determinada aula
-#     idModulo = Column(Integer, primary_key=True, nullable=False)
-#     definicionModulo=Column(String)
-#     horarioInicio = Column(Time, nullable=False)
-#     horarioFin = Column(Time, nullable=False)
-#     dia = Column(Date, nullable=False)
-#     aula = Column(String, nullable=False)
-
-#     def __init__(self,horaIni,horaFin,dia,aula,defMod):
-#         self.horarioInicio = horaIni
-#         self.horarioFin = horaFin
-#         self.dia=dia
-#         self.aula=aula
-#         self.definicionModulo=defMod
-
-#     def __repr__(self):
-#         return f'Modulo: {self.idModulo} horarios: {self.horarioInicio} {self.horarioFin} dia: {self.dia} aula: {self.aula} def:{self.definicionModulo}'
-
-# assosiation_table_materia_comision = Table('assosiation_materia_comision',Base.metadata,
-#     Column('materias_id',Integer,ForeignKey('materias.idMaterias')),
-#     Column('comisiones_id',Integer,ForeignKey('right.id'))
-#     )
-
-# '''Falta clase alumno materia'''
+Base.metadata.create_all(engine)
